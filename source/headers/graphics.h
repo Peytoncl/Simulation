a3ebca04297630e26322db97e4fc4d25525bfe8c
Peytoncl <peytonthegreat0@gmail.com>
2025-03-27 01:22:21 -0400

    restructure

--- source/headers/graphics.h
+++ source/headers/graphics.h
@@ -1,7 +1,12 @@
 #ifndef GRAPHICS_H
 
+#define GRAPHICS_H
+
 #define WINDOW_W 800
 #define WINDOW_H 600
+
+#define TARGET_FPS 60
+#define FRAME_TIME (1000.0 / TARGET_FPS) // 1000ms / 60fps = 16.67ms per frame
 
 typedef struct 
 {
